d13fc0ff5842be3b2350ea61a945adcb6a6ea0a3
Mukesh Vaithiya <mukesh312007@gmail.com>
2026-04-16 19:37:33 +0530

    Time: 1 ms (9.47%), Space: 12.4 MB (69.71%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def myPow(self, x, n):
+        if n < 0:
+            x = 1 / x
+            n = -n
+
+        def fast_pow(x, n):
+            if n == 0:
+                return 1.0
+
+            half = fast_pow(x, n // 2)
+
+            if n % 2 == 0:
+                return half * half
+            else:
+                return half * half * x
+
+        return fast_pow(x, n)
