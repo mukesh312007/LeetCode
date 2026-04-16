0c13ca0ee6dedc5b69ff85333dc967452aed63df
Mukesh Vaithiya <mukesh312007@gmail.com>
2026-04-16 19:33:37 +0530

    Time: 11 ms (38.81%), Space: 13 MB (82.17%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def jump(self, nums):
+        n = len(nums)
+        if n <= 1:
+            return 0
+
+        jumps = 0
+        current_end = 0
+        farthest = 0
+
+        for i in range(n - 1):
+            farthest = max(farthest, i + nums[i])
+
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+
+        return jumps
